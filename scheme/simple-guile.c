3f607bf2e3958714b678e1b187183eaa9dec0c46
Eric Wang <wangchaogo1990@gmail.com>
2014-05-30 10:35:42 -0400

    add a function to make it callable in scheme

--- scheme/simple-guile.c
+++ scheme/simple-guile.c
@@ -2,8 +2,19 @@
  * When execute, it produces a complete Guile interpreter
  * gcc simple-guile.c -lguile
 */
+#include <stdlib.h>
 #include <libguile.h>
+
+static SCM my_hostname (void){
+	char *s = getenv ("HOSTNAME");
+	if (s == NULL)
+		return SCM_BOOL_F;
+	else
+		return scm_from_locale_string (s);
+}
+
 static void inner_main (void *closure, int argc, char **argv) {
+	scm_c_define_gsubr ("my-hostname", 0, 0, 0, my_hostname);
 	/* preparation */
 	scm_shell (argc, argv);
 	/* after exit */
