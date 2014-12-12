9cc6ae0fc40fc3aa558855c77f689975cf9120ed
Eric Wang <wangchaogo1990+fedora.lab@gmail.com>
2014-12-12 22:55:52 +0800

    add neural network package for r and update others

--- conf/package.r
+++ conf/package.r
@@ -1,8 +1,20 @@
 ## sudo R --no-save -f soft.r
-install.packages("kknn")
+
 install.packages("ElemStatLearn")
+
+## machine learning
+## 	 k-Nearest Neighbors
+install.packages("kknn")
+## 	svm
 install.packages("e1071")
 install.packages("kernlab")
+## 	random forest
+install.packages("randomForest")
+## 	neural network
+install.packages("nnet")
+install.packages("neuralnet")
+
+## data base
 install.packages("DBI")
 install.packages("RMySQL")
 
