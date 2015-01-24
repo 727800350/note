d5e036ad2fc737e28f15cc7821b8450e7f9ca98f
Eric Wang <wangchaogo1990@gmail.com>
2015-01-24 16:18:48 +0800

    update package for R

--- conf/package.r
+++ conf/package.r
@@ -1,31 +1,36 @@
 ## sudo R --no-save -f soft.r
 
-install.packages("ElemStatLearn")
-install.packages("clusterGeneration")
-install.packages("devtools")
-install.packages("scales")
-install.packages("reshape")
-install.packages("reshape2")
+install.packages("ElemStatLearn") ## for the book: "The Elements of Statistical Learning, Data Mining, Inference, and Prediction"
+install.packages("devtools") ## Tools to Make Developing R Packages Easier
+
+install.packages("scales") ## Scale functions for graphics
+install.packages("reshape") ## Flexibly reshape data
+install.packages("reshape2") ## Flexibly Reshape Data: A Reboot of the Reshape Package
+
+## statistics
+install.packages("Hmisc") ## Harrell Miscellaneous
+install.packages("pastecs") ## Package for Analysis of Space-Time Ecological Series
+install.packages("psych") ## Procedures for Psychological, Psychometric, and Personality Research
+install.packages("doBy") ## Groupwise statistics, LSmeans, linear contrasts, utilities
+install.packages("vcd")  ## Visualizing Categorical Data
 
 ## machine learning
-## 	 k-Nearest Neighbors
-install.packages("kknn")
-## 	svm
-install.packages("e1071")
-install.packages("kernlab")
-## 	random forest
-install.packages("randomForest")
-## 	neural network
-install.packages("nnet")
-install.packages("neuralnet")
-install.packages("RSNNS")
+install.packages("kknn") ## k-Nearest Neighbors
+install.packages("e1071") ## Misc Functions of the Department of Statistics (e1071), TU Wien
+## Functions for latent class analysis, short time Fourier transform, fuzzy clustering, SVM, shortest path, bagged clustering, naive Bayes classifier, ...
+install.packages("clusterGeneration") ## random cluster generation (with specified degree of separation)
+install.packages("kernlab") ## Kernel-based Machine Learning Lab
+install.packages("randomForest") ## random forest
+install.packages("nnet") ## Feed-forward Neural Networks and Multinomial Log-Linear Models
+install.packages("neuralnet") ## Training of neural networks
+install.packages("RSNNS") ## Neural Networks in R using the Stuttgart Neural Network Simulator (SNNS)
 
 ## data base
-install.packages("DBI")
+install.packages("DBI") ## R Database Interface
 install.packages("RMySQL")
 
 ## need at first sudo yum install -y curl libcurl libcurl-devel
-install.packages("RCurl")
+install.packages("RCurl") ## General network (HTTP/FTP/...) client interface for R
 
 ## graphics
-install.packages("ggplot2")
+install.packages("ggplot2") ## An implementation of the Grammar of Graphics
