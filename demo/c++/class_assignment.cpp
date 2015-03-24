bd0829fd3c558574f998eba4d67f334162401c03
Eric Wang <wangchaogo1990@gmail.com>
2015-03-24 16:41:40 +0800

    add inheritance class assignment and virtual class construnctor demo

--- demo/c++/class_assignment.cpp
+++ demo/c++/class_assignment.cpp
@@ -23,20 +23,32 @@ public:
     }
 };
 
-void fun(B0 *ptr){
-    ptr->display();
-}
-
 int main(){ 
     B0 b0;
-    B1 b1;
-    B2 b2;
-    fun(&b0);
-    b0 = b1;
-    fun(&b0);
-    b0 = b2;
-    fun(&b0);
+	b0.display();
 
+    B1 b1;
+	b1.display();
+
+    B2 b2;
+	b2.display();
+
+// 	B0是B1 的基类, 也就是说B1中包含B0的成员, 所以可以把B1的实例赋值给B0的实例, 也就是说将B1 的实例当成B0使用
+    b0 = b1;
+	b0.display();
+
+    b0 = b2;
+	b0.display();
     return 0; 
 }
+
+/**
+ * output:
+ *
+ * B0::display()
+ * B1::display()
+ * B2::display()
+ * B0::display()
+ * B0::display()
+**/
 
