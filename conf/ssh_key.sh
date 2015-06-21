2b2de8fa66709282476902990a6b631dadc69ca7
root <root@localhost.localdomain>
2015-06-21 20:27:10 +0800

    update ssh key

--- conf/ssh_key.sh
+++ conf/ssh_key.sh
@@ -2,8 +2,14 @@
 ## generate ssh key
 set -x
 
-cd ~/.ssh
+cd ~
+if [ ! -d .ssh ]
+then
+	mkdir .ssh
+fi
+cd .ssh
 ls -l
+
 if [ -f id_rsa -o -f id_rsa.pub ]
 then
 	echo "id_rsa exists"
@@ -13,3 +19,5 @@ fi
 ssh-keygen -t rsa -C "wangchaogo1990@gmail.com" -N thinkpad -f id_rsa 1>/dev/null
 cat id_rsa.pub
 cd ~
+echo "test github connection: ssh -T git@github.com"
+
