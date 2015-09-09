8d2be7b5ff5f171f5af93e6e1f83e5ea41d961fd
Eric Wang <wangchaogo1990@gmail.com>
2015-09-09 21:18:10 +0800

    update parse args

--- demo/scheme/parse_args.scm
+++ demo/scheme/parse_args.scm
@@ -1,19 +1,25 @@
-#! /usr/local/bin/guile -s
-!#
+":"; exec guile $0 "$@"
 (define cmm (command-line))
-(display "Program name: ")
-(display (car cmm))  
+(display (string-append "Program name: " (car cmm)))
 (newline)
+
+(define loop 
+	(lambda (count len obj)
+		(if (<= count len)
+			(begin
+				(display "argument ")
+				(display count)
+				(display " : ")
+				(display (list-ref obj (- count 1)))
+				(newline)
+				(set! count (+ count 1))
+				(loop count len obj)
+			)
+		)
+	)
+)
+
 (define args (cdr cmm))
 (define long (length args))
-(define loop (lambda (count len obj)
-	(if (<= count len)
-		(begin
-			(display "argument ")
-			(display count)
-			(display " : ")
-			(display (list-ref obj (- count 1)))
-			(newline)
-			(set! count (+ count 1))
-			(loop count len obj)))))
 (loop 1 long args)
+
