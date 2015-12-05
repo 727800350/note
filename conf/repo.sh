7cc50d2d4ac91e0dbea9ea12cdfce64cc4fc6422
Eric Wang <wangchaogo1990@gmail.com>
2015-12-05 20:06:42 +0800

    update repo and soft

--- conf/repo.sh
+++ conf/repo.sh
@@ -1,170 +1,38 @@
 #!/bin/bash
-## take care of repos
 set -x
 
-source ./common.env
+source ${HOME}/git/note/conf/func.sh || exit 1
 
-## the path should not contain backspace, even with back slash
-if [ $# -eq 1 ]
-then
-	pkgs=$1
-else
-	echo "wrong parameter"
-	echo "usage: $0 path"
-	exit 1
-fi
+version="7"
 
-## the repos is the lowest(99)
-yum list installed | grep ^yum-plugin-priorities\. >&2
-if [ $? -ne 0 ]
-then
-	yum install -y yum-plugin-priorities
-fi
+yum install -y yum-plugin-priorities
+CHK_RET WARNING "install yum plugin priorities error"
 
-yum list installed | grep ^createrepo\. >&2
-if [ $? -ne 0 ]
-then
-	yum install -y installer createrepo
-fi
+yum -y localinstall http://pkgs.repoforge.org/rpmforge-release/rpmforge-release-0.5.3-1.el${version}.rf.x86_64.rpm
+CHK_RET FATAL "install repo rpmforge error"
 
-local="/yum/"
-if [ ! -d ${local} ]
-then
-	sudo mkdir -p ${local}
-fi
+yum -y localinstall http://www.elrepo.org/elrepo-release-7.0-2.el7.elrepo.noarch.rpm
+CHK_RET FATAL "install repo elrepo error"
 
-target=${local}/`basename ${pkgs}`
-ls -l ${target} | head >&2
-ls -l ${target} | grep -v rpm | head >&2
-if [ ! -e ${target}/TRANS.TBL ]
-then
-	num=`ls -l ${pkgs} | wc -l | awk '{print $1}'`
-	if [ ${num} -gt 0 ]
-	then
-		cp -r ${pkgs} ${local} || exit 1
-		createrepo $target
-	else
-		echo "no rpm packages in ${pkgs}" >&2
-	fi
-else
-	echo "local repo already exists" >&2
-fi
+yum -y localinstall http://dl.fedoraproject.org/pub/epel/epel-release-latest-${version}.noarch.rpm
+CHK_RET FATAL "install repo epel error"
 
-cd /etc/yum.repos.d
+yum-config-manager --add-repo=https://copr.fedoraproject.org/coprs/mosquito/myrepo/repo/epel-${version}/mosquito-myrepo-epel-${version}.repo
+CHK_RET FATAL "install repo mosquite-myrepo-epel error"
 
-repo="localyum.repo"
-if [ -e ${target}/TRANS.TBL -a ! -e ${repo} ]
-then
-	echo "[localyum]" > ${repo}
-	echo "priority=1" >> ${repo}
-	echo "Name=localyum" >> ${repo}
-	echo "baseurl=file://${target}" >> ${repo}
-	echo "enable=1" >> ${repo}
-	echo "gpgcheck=0" >> ${repo}
-fi
+yum -y localinstall http://li.nux.ro/download/nux/dextop/el${version}/x86_64/nux-dextop-release-0-1.el${version}.nux.noarch.rpm
+CHK_RET FATAL "install repo nux-desktop-release error"
 
-function update_repo(){
-	repo=$1
-	priority=$2
-	name=$3
+yum -y localinstall http://download1.rpmfusion.org/nonfree/el/updates/${version}/x86_64/rpmfusion-nonfree-release-${version}-1.noarch.rpm
+CHK_RET FATAL "install repo rpmfusion-nonfree error"
 
-	if [ ! -e ${repo} ]
-	then
-		echo "${repo} does not exist" >&2
-		return 1
-	fi
+yum -y localinstall http://download1.rpmfusion.org/free/el/updates/${version}/x86_64/rpmfusion-free-release-${version}-1.noarch.rpm
+CHK_RET FATAL "install repo rpmfusion-free error"
 
-	cat ${repo} | grep priority=${priority}
-	if [ $? -ne 0 ]
-	then
-		sed -i "/^\[${name}\]/a priority=${priority}" ${repo}
-	fi
-}
-
-suffix=`date +%Y%m%d_%H_%M_%S`
-if [ ${os} = "centos" ]
-then
-	## local repo
-	update_repo CentOS-Base.repo 2 base
-	update_repo CentOS-Base.repo 2 updates
-
-	if [ ! -e mosquito-myrepo-epel-${version}.repo ]
-	then
-		yum-config-manager --add-repo=https://copr.fedoraproject.org/coprs/mosquito/myrepo/repo/epel-${version}/mosquito-myrepo-epel-${version}.repo
-	fi
-	update_repo mosquito-myrepo-epel-${version}.repo 3 mosquito-myrepo
-
-	## 默认的Centos 7不带能够播放MP3和视频文件的解码器支持, 默认repo和后来自己加的源都不带, 需要nux-dextop repo, 先把epel 的repo弄好
-	if [ ! -e nux-dextop.repo ]
-	then
-		yum localinstall -y http://li.nux.ro/download/nux/dextop/el${version}/x86_64/nux-dextop-release-0-1.el${version}.nux.noarch.rpm
-	fi
-	update_repo nux-dextop.repo 4 nux-dextop
-
-	if [ ! -e nux-dextop.repo ]
-	then
-		yum localinstall -y http://dl.fedoraproject.org/pub/epel/epel-release-latest-${version}.noarch.rpm
-	fi
-	update_repo epel.repo 3 epel
-
-	if [ ! -e nux-dextop.repo ]
-	then
-		yum localinstall -y http://download1.rpmfusion.org/nonfree/el/updates/${version}/x86_64/rpmfusion-nonfree-release-${version}-1.noarch.rpm
-	fi
-	update_repo rpmfusion-nonfree.repo 4 rpmfusion-nonfree
-
-	if [ ! -e nux-dextop.repo ]
-	then
-		yum localinstall -y http://download1.rpmfusion.org/free/el/updates/${version}/x86_64/rpmfusion-free-release-${version}-1.noarch.rpm
-	fi
-	update_repo rpmfusion-free.repo 4 rpmfusion-free
-
-	if [ ! -e nux-dextop.repo ]
-	then
-		yum localinstall -y http://pkgs.repoforge.org/rpmforge-release/rpmforge-release-0.5.3-1.el${version}.rf.x86_64.rpm
-	fi
-	update_repo rpmforge.repo 4 rpmforge
-
-	if [ ${version} -eq 7 ]
-	then
-		if [ ! -e nux-dextop.repo ]
-		then
-			yum -y localinstall http://www.elrepo.org/elrepo-release-7.0-2.el7.elrepo.noarch.rpm
-		fi
-		update_repo elrepo.repo 4 elrepo
-	elif [ ${version} -eq 6 ]
-	then
-		if [ ! -e nux-dextop.repo ]
-		then
-			yum -y localinstall http://www.elrepo.org/elrepo-release-6-6.el6.elrepo.noarch.rpm
-		fi
-		update_repo elrepo.repo 4 elrepo
-	else
-		echo "${os} ${version} not supported"
-	fi
-elif [ ${os} = "fedora" ]
-then
-	## local repo
-	update_repo fedora.repo 2 fedora
-	update_repo fedora.repo 2 updates
-
-	if [ ! -e nux-dextop.repo ]
-	then
-		yum -y localinstall http://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-	fi
-	update_repo epel.repo 3 epel
-else
-	echo "${os} not supported" >&2
-fi
-
-if [ -e google-chrome.repo ]
-then
-	sed -i "s/enabled=1/enabled=0/g" google-chrome.repo
-fi
+yum repolist
 
 yum clean all
 yum makecache
-cd ~
 
 exit 0
 
--- conf/software.sh
+++ conf/software.sh
@@ -1,211 +1,47 @@
 #!/bin/bash
-## install software for new os
 set -x
 
-source ./common.env
+source ${HOME}/git/note/conf/func.sh || exit 1
 
-installer tmux
+yum -y install fuse-ntfs-3g
+CHK_RET FATAL "fuse-ntfs-3g error"
 
-installer samba
-chkconfig --level 35 smb on
+yum -y install unrar
+CHK_RET FATAL "unrar error"
 
-## screen capture
-installer shutter
+## bash ./install_advp
+## CHK_RET WARNING "advp error"
 
-## video and audio convertor
-installer ffmpeg
+## bash ./install_chrome
+## CHK_RET FATAL "chrome error"
 
-## text convertion between windows and linux
-installer unix2dos dos2unix
-installer pdfgrep
+yum -y localinstall http://kdl.cc.ksosoft.com/wps-community/download/a19/wps-office-9.1.0.4975-1.a19p1.x86_64.rpm
+CHK_RET FATAL "wps error"
 
-## programming
-installer gcc gcc-c++ gdb cmake
-installer golang
-installer valgrind
-installer strace
-installer libasan libasan-static
-installer flex byacc
+yum -y install unix2dos dos2unix
+CHK_RET FATAL "unix2dos error"
 
-## zmodem/ymodel/xmodem package
-installer lrzsz
+yum -y install pdfgrep
 
-installer unrar
-installer mlocate
+yum -y install cmake clang installer golang valgrind
+CHK_RET FATAL "dev error"
 
-installer mariadb-server mariadb mariadb-devel
-installer glib2 glib2-devel
-installer boost
+yum -y install mariadb-server mariadb-devel
+CHK_RET FATAL "mysql error"
 
-## php
-installer php
-installer php-mysql php-gd php-imap php-ldap php-odbc php-pear php-xml php-xmlrpc
+yum -y install php php-mysql php-gd php-imap php-ldap php-odbc php-pear php-xml php-xmlrpc
+CHK_RET FATAL "php error"
 
-installer phpMyAdmin
-## php data file in /usr/share/phpMyAdmin
-## 访问权限的设置在/etc/httpd/conf.d/phpMyAdmin.conf
-## Apache 2.4+ uses "Require all granted" instead of "Allow from all"
-## 为了让局域网内的其他机器能够访问网站, 将防火墙关闭掉
-if [ ${type} = "server" ]
-then
-	if [ ${os} = "fedora" ]
-	then
-		service firewalld stop
-		setenforce 0
-	elif [ ${os} = "centos" ]
-	then
-		service iptables stop
-		setenforce 0 #enforce 的默认值在/etc/selinux/config, permissive = 0, enforcing = 1
-	else
-		echo "${os} not supported" >&2
-	fi
-fi
-## 当在/var/www/html/ 中创建了一个指向 /home/eric 的symlink, 可能在浏览器中访问时, 提示权限问题
-## Apache 需要这个目录以及上级目录的rx权限
+yum -y install python-pip numpy scipy python-matplotlib
+CHK_RET FATAL "python lib error"
 
-## project
-installer libpcap libpcap-devel
+yum -y install R
 
-installer wireshark ## no gui
-if [ ${type} = "desktop" ]
-then
-	installer wireshark-gnome
-fi
+yum -y install wiznote ## Stable version  
 
-## Berkeley Internet Name Domain (BIND) DNS (Domain Name System)
-installer bind 
-
-## Network exploration tool and security scanner
-installer nmap 
-
-## A whois client that accepts both traditional and finger-style queries
-installer jwhois 
-
-## ntopng prerequisite
-installer libxml2-devel sqlite-devel
-
-### zlog
-if [ ! -e /usr/local/include/zlog.h ]
-then
-	wget -c https://codeload.github.com/HardySimpson/zlog/tar.gz/latest-stable
-	if [ $? -eq 0 -a -e latest-stable ]
-	then
-		tar -xvf latest-stable
-		cd zlog-latest-stable
-		make
-		make install
-		cd ../
-		rm -rf latest-stable zlog-latest-stable
-	fi
-fi
-
-### local libs
-if [ ! -e /etc/ld.so.conf.d/local_lib.conf -o ! -e /etc/ld.so.conf.d/local_lib64.conf ]
-then
-	cd /etc/ld.so.conf.d
-	echo /usr/local/lib > local_lib.conf
-	echo /usr/local/lib64 > local_lib64.conf
-	ldconfig
-else
-	echo "/etc/ld.so.conf.d/local_lib.conf and /etc/ld.so.conf.d/local_lib64.conf already exist" >&2
-fi
-
-<<COM
-## octave
-## will install hdf5, hdf5-static and hdf5-devel
-installer hdf5-static 
-sudo rpm -ivh epstool-3.08-7.fc21.x86_64.rpm
-installer octave
-COM
-
-## python
-installer numpy scipy
-installer python-pip 
-
-function pip_install(){
-	for soft in $@
-	do
-		## check
-		pip list | grep ${soft}
-		## the  exit  status  is 0 if selected lines are found and 1 otherwise.
-		if [ $? -ne 0 ]
-		then
-			echo "${soft} has not been installed"
-			## install
- 			pip install ${soft}
-		fi
-	done
-}
-
-pip_install xlutils
-## 	you-get和youtube-dl <https://github.com/rg3/youtube-dl>是两个用于从视频网站上下载视频文件的工具。其中，后者支持的网站更多，但前者对国内的视频网站支持更好。
-if [ -e /usr/bin/python3 ]
-then
-	pip_install you-get ## needs python 3
-fi
-pip_install youtube-dl
-
-installer mysql-connector-python
-
-installer wiznote      # Stable version  
-
-installer gstreamer gstreamer-plugins-base gstreamer-plugins-good gstreamer-plugins-bad-free
-
-installer alacarte ## set desktop menu
-
-## cpdf是一个跨平台的PDF处理工具，可以完成常见的PDF合并、切割、加密解密、书签、水印等功能。
-## 下载已编译好的二进制包，解压，并将与自己的平台对应的二进制文件复制到${HOME}/bin目录下即可使用。
-cd /home/
-user=`ls | grep -v lost+found`
-cd ${user}
-if [ ! -d bin ]
-then
-	mkdir bin
-fi
-if [ ! -e bin/cpdf -o ! -e bin/smpdf ]
-then
-	wget -c https://github.com/coherentgraphics/cpdf-binaries/archive/master.zip
-	unzip cpdf-binaries-master.zip
-
-	cp cpdf-binaries-master/Linux-Intel-64bit/cpdf bin/
-	cp cpdf-binaries-master/cpdfmanual.pdf bin/
-
-	cp cpdf-binaries-master/LosslessPDFCompressor/Linux64/smpdf bin/
-	cp cpdf-binaries-master/LosslessPDFCompressor/smpdfmanual.pdf bin/
-	chmod +x bin/cpdf bin/smpdf
-	rm -rf cpdf-binaries-master.zip cpdf-binaries-master
-fi
-
-<<COM
-## mutt
-sudo yum install -y mutt msmtp getmail procmail lynx elinks
-
-## bt
-sudo yum install -y transmission
-
-## graphviz, dot
-sudo yum install graphviz
-sudo dot -c  ## configure plugins
-sudo yum install graphviz-gd ## support for png, jpg, jpeg etc
-COM
-
-## http://seisman.info/fcitx-for-centos-7.html
-## installer fcitx-googlepinyin fcitx-cloudpinyin
-
-## wps office
-yum list installed | grep ^wps-office\\.
-if [ $? -ne 0 ]
-then
-	yum localinstall -y http://kdl.cc.ksosoft.com/wps-community/download/a18/wps-office-9.1.0.4961-1.a18p1.x86_64.rpm
-fi
-
-## R language
-installer R
-installer curl libcurl libcurl-devel
-
-## 不含参数的library()函数会显示已安装的R包列表
-## installed.packages()[,c("Package","Version")]
-## nohup Rscript ./package.r 1>r.log 2>&1 &
+yum -y install alacarte ## set desktop menu
+yum -y install ffmpeg ## video and audio convertor
+yum -y install shutter ## screen capture
 
 exit 0
 
