0b30af255c6c3dff3c9dde6f40b9f115e1986fa7
Eric Wang <wangchaogo1990@gmail.com>
2016-02-01 22:32:53 +0800

    add upside bar plot

--- graph/matplotlib/bar.py
+++ graph/matplotlib/bar.py
@@ -4,8 +4,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 data = [[5., 25., 50., 20.],
-        [4., 23., 51., 17.],
-        [6., 22., 52., 19.]]
+	[4., 23., 51., 17.],
+	[6., 22., 52., 19.]]
 
 ## hard code of position
 def plot_static():
@@ -22,11 +22,33 @@ def plot_dynamic():
 	color_list = ['b', 'g', 'r']
 	gap = .8 / len(data) ## number of rows
 	for i, row in enumerate(data):
-	    X = np.arange(len(row))
-            color = color_list[i % len(color_list)]
-	    plt.bar(X + i * gap, row, width = gap, color = color)
+		X = np.arange(len(row))
+		color = color_list[i % len(color_list)]
+		plt.bar(X + i * gap, row, width = gap, color = color)
+	plt.show()
+
+## plot B upon A
+def plot_upside():
+	A = [5., 30., 45., 22.]
+	B = [5., 25., 50., 20.]
+	X = range(4)
+	plt.bar(X, A, color = 'b')
+	plt.bar(X, B, color = 'r', bottom = A)
+	plt.show()
+
+def plot_upside_dynamic():
+	data = np.array([[5., 30., 45., 22.],
+			[5., 25., 50., 20.],
+			[1., 2., 1., 1.]])
+	color_list = ['b', 'g', 'r']
+	X = np.arange(data.shape[1])
+	for i in range(data.shape[0]):
+		plt.bar(X, data[i], bottom = np.sum(data[:i], axis = 0), color = color_list[i % len(color_list)])
 	plt.show()
 
 ## plot_static()
-plot_dynamic()
+## plot_dynamic()
+
+## plot_upside()
+plot_upside_dynamic()
 
