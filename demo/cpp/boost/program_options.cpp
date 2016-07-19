7ede2ccebd0d91ade3ed0dd8b40145c6538a7571
Eric Wang <wangchaogo1990@gmail.com>
2016-07-19 23:03:27 +0800

    update boost program options

--- demo/cpp/boost/program_options.cpp
+++ demo/cpp/boost/program_options.cpp
@@ -1,19 +1,17 @@
 /************************************************************
-makefile:
-g++ -c option.cpp
-g++ -o a.out option.o -L/usr/local/lib -lboost_program_options
+g++ program_options.cpp -L/usr/local/lib -lboost_program_options
 
 results:
 run:
-	./a.out --apples 20 --orangs 30
+	./a.out --apple 20 --orange 30
 	./a.out -a 20 -o 30 --name eric
 	./a.out --help
 
 If using parse config file
 	## comment: apples_oranges.cfg
 	name=eric
-	apples=10
-	oranges=20
+	apple=10
+	orange=20
 
 	./a.out
 ************************************************************/
@@ -25,40 +23,35 @@ If using parse config file
 namespace opt = boost::program_options;
 
 int main(int argc, char *argv[]){
-    opt::options_description desc("All options");
-    desc.add_options()
-        ("apples,a", opt::value<int>()->default_value(10), "apples that you have")
-        ("oranges,o", opt::value<int>(), "oranges that you have")
-        ("name", opt::value<std::string>(), "your name")
-        ("help", "produce help message")
-    ;
+	opt::options_description desc("All options");
+	desc.add_options()
+		("apple,a", opt::value<int>()->default_value(10), "apples that you have")
+		("orange,o", opt::value<int>(), "oranges that you have")
+		("name", opt::value<std::string>(), "your name")
+		("help,h", "produce help message")
+	;
 
-    opt::variables_map vm;
- // Parsing command line options and storing values to 'vm'
+	opt::variables_map vm;
 	opt::store(opt::parse_command_line(argc, argv, desc), vm);
-    // We can also parse environment variables using 'parse_environment' method
+	opt::notify(vm);
 
-// void notify(const boost::any & value_store) const;
-// Called when final value of an option is determined.
-    opt::notify(vm);
+	if(vm.count("help")) {
+		std::cout << desc << "\n";
+		return 0;
+	}
 
-    if(vm.count("help")) {
-        std::cout << desc << "\n";
-        return 1;
-    }
+	try {
+		opt::store(opt::parse_config_file<char>("apples_oranges.conf", desc), vm);
+		opt::notify(vm);
+	} catch (const opt::reading_file& e){
+		std::cout << e.what() << std::endl;
+	}
 
-    // Adding missing options from "aples_oranges.cfg" config file.
-    // You can also provide an istreamable object as a first parameter for 'parse_config_file'
-    // 'char' template parameter will be passed to underlying std::basic_istream object
-    try {
-      opt::store(opt::parse_config_file<char>("apples_oranges.cfg", desc), vm);
-    } catch (const opt::reading_file& e){
-        std::cout << "Failed to open file 'apples_oranges.cfg': " << e.what();
-    }
-    opt::notify(vm);
-    if (vm.count("name")){
-      std::cout << "Hi," << vm["name"].as<std::string>() << "!\n";
-    }
-    std::cout << "Fruits count: " << vm["apples"].as<int>() + vm["oranges"].as<int>() << std::endl;
-    return 0;
+	if (vm.count("name")){
+		std::cout << "Hi," << vm["name"].as<std::string>() << "!\n";
+	}
+	std::cout << "Fruits count: " << vm["apple"].as<int>() + vm["orange"].as<int>() << std::endl;
+
+	return 0;
 }
+
