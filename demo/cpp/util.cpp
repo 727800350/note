356c62c36faa1bbaf14bcbc7c5c6e220efe396e7
Eric Wang <wangchaogo1990@gmail.com>
2016-11-22 17:15:57 +0800

    use const char

--- demo/cpp/util.cpp
+++ demo/cpp/util.cpp
@@ -1,17 +1,22 @@
 #include <stdio.h>
 #include <string.h>
-#include <vector.h>
+#include <string>
+#include <vector>
 
-std::vector<std::string> split(char *str, char *delim){
+std::vector<std::string> split(const char *str, const char *delim){
 	std::vector<std::string> toks;
 	char *p = NULL;
 
-	p = strtok(str, delim);
+	char *s = new char[strlen(str) + 1];
+	memcpy(s, str, strlen(str) + 1);
+
+	p = strtok(s, delim);
 	toks.push_back(std::string(p));
 	while((p = strtok(NULL, delim)) != NULL){
 		toks.push_back(std::string(p));
 	}
 
+	delete []s;
 	return toks;
 }
 
@@ -19,7 +24,6 @@ int main(){
 	char s[] = "- This, a sample string.";
 	char *delim = " ,.-";
 	std::vector<std::string> toks = split(s, delim);
-
 	for(unsigned int i = 0; i < toks.size(); i++){
 		fprintf(stderr, "%dth token: %s\n", i, toks[i].c_str());
 	}
