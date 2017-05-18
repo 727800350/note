ef71dd145fb1eead62999f44fea03bf7f6883c8c
Eric Wang <wangchaogo1990@gmail.com>
2017-05-18 21:42:43 +0800

    update merger

--- demo/python/merger.py
+++ demo/python/merger.py
@@ -40,39 +40,47 @@ flag_0: whether record from data source with flag 0 has value
 flag_1: whether record from data source with flag 1 has value
 sep: field seperator between value from data source 0 and data source 1 if both flag_0 and flag_1 are True, with space as default
 '''
-flag_0 = False
+flag_0 = True
 flag_1 = True
 sep = ":"
 
-def process_dict(dictionary, key):
-    """ process a dict with only a key """
-    if key in dictionary:
-        value = dictionary[key]
-        if flag_0:
-            if len(value) == 1:
-                print >> sys.stdout, ("%s\t%d\t%s" % (key, 0, value[0]))
-                del dictionary[key]
+def compose_fv(flag, v0, v1):
+    """ compose flag and value when found """
+    if flag_0:
+        if flag_1:
+            if v1 is None:
+                return flag + '\t' + v0
             else:
-                if flag_1:
-                    for i in range(1, len(value)):
-                        print >> sys.stdout, ("%s\t%d\t%s" % (key, 1, value[0] + sep + value[i]))
-                    del dictionary[key]
-                else:
-                    print >> sys.stdout, ("%s\t%d\t%s" % (key, 1, value[0]))
-                    del dictionary[key]
+                return flag + '\t' + v0 + sep + v1
         else:
-            if len(value) == 0:
-                print >> sys.stdout, ("%s\t%d" % (key, 0))
-                del dictionary[key]
+            return flag + '\t' + v0
+    else:
+        if flag_1:
+            if v1 is None:
+                return flag
             else:
-                if flag_1:
-                    for element in value: 
-                        print >> sys.stdout, ("%s\t%d\t%s" % (key, 1, element))
-                    del dictionary[key]
-                else:
-                    print >> sys.stdout, ("%s\t%d" % (key, 1))
-                    del dictionary[key]
+                return flag + '\t' + v1
+        else:
+            return flag
 
+def process_dict(d, key):
+    """ process a dict with only a key """
+    if key + '_0' in d:
+        vs0 = d[key + '_0']
+        if key + '_1' in d:
+            vs1 = d[key + '_1']
+            for v0 in vs0:
+                for v1 in vs1:
+                    print >> sys.stdout, "%s\t%s" % (key, compose_fv('1', v0, v1))
+        else:
+            ## only v0
+            for v0 in vs0:
+                print >> sys.stdout, "%s\t%s" % (key, compose_fv('0', v0, None))
+    ## clean
+    if key + '_0' in d:
+        del d[key + '_0']
+    if key + '_1' in d:
+        del d[key + '_1']
 
 def main():
     """ main func """
@@ -84,6 +92,7 @@ def main():
     flag = ''
     value = ''
     
+    num = 0
     for line in sys.stdin:
         ## parser the line
         line = line.strip()
@@ -93,25 +102,39 @@ def main():
 
         if flag == '0':
             if flag_0:
-                value = terms[2]
-        elif flag == '1':
-            if flag_1:
+                if len(terms) < 3:
+                    print >> sys.stderr, "%d %s format error" % (num, line)
+                    num = num + 1
+                    continue
                 value = terms[2]
             else:
-                value = ""
+                value = ''
+        elif flag == '1':
+            if flag_1:
+                if len(terms) < 3:
+                    print >> sys.stderr, "%d %s format error" % (num, line)
+                    num = num + 1
+                    continue
+                value = terms[2]
+            else:
+                value = ''
         else:
             print >> sys.stderr, "flag " + flag + " not supported"
             sys.exit(1)
         
         ## a new key
-        if key not in d and flag == '0':
-            d[key] = []
-            if flag_0:
-                d[key].append(value)
-
+        if key + '_0' not in d:
+            if flag == '0':
+                d[key + '_0'] = []
+                d[key + '_0'].append(value)
+        else:
         ## key aleady exists
-        if key in d and flag == '1':
-            d[key].append(value)
+            if flag == '1':
+                if key + '_1' not in d:
+                    d[key + '_1'] = []
+                d[key + '_1'].append(value)
+            else:
+                d[key + '_0'].append(value)
     
         ## the first line
         if key_old == '':
@@ -125,10 +148,13 @@ def main():
             key_old = key
             flag_old = flag
             value_old = value
+
+        num = num + 1 ## end of for line in sys.stdin
     
     ## the last possible key
-    for k in d.keys():
-        process_dict(d, k)
+    process_dict(d, key)
+
+    print >> sys.stderr, "total input num: %d" % num
 
 if __name__ == "__main__":
     main()
