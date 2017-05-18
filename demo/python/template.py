f1e267c9713a2fa42858bbb267a44732faeb859a
Eric Wang <wangchaogo1990@gmail.com>
2017-05-18 11:41:49 +0800

    add process

--- demo/python/template.py
+++ demo/python/template.py
@@ -4,18 +4,32 @@
 import sys
 import logging
 import argparse
+import time
 
 ## debug, info, warnning, error, critical
 logging.basicConfig(level = logging.DEBUG, format = "%(levelname)s %(asctime)s [%(filename)s][%(lineno)d][%(funcName)s] %(message)s")
 log = logging.getLogger()
+
+def process(line):
+    print >> sys.stdout, line
 
 def main():
     parser = argparse.ArgumentParser(description = 'python template')
     parser.add_argument('-n', action = 'store', dest = 'num', default = 0, type = int, help = 'number')
     parser.add_argument('-f', action = 'store', dest = 'dict', default = 'dict.txt', type = str, help = 'dict file')
     arg = parser.parse_args()
-    print >> sys.stdout, "hello world"
-    log.debug(arg.num)
+
+    num = 0
+
+    time_start = time.time()
+    for line in sys.stdin:
+        line = line.strip()
+        process(line)
+        num = num + 1
+    time_total = time.time() - time_start
+
+    qps = num / time_total
+    log.info("num: %d process time: %fs qps: %d" % (num, time_total, qps))
 
 if __name__ == "__main__":
     main()
