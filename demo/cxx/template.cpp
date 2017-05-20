f6d8bdc930de5e00eed78ce00a143e538da37b8a
Eric Wang <wangchaogo1990@gmail.com>
2017-05-20 18:01:18 +0800

    update c++ template

--- demo/cxx/template.cpp
+++ demo/cxx/template.cpp
@@ -1,15 +1,24 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdint.h>
 #include <unistd.h>
+
+const int max_kl = 1024;
+const int max_vl = 10 * 1024 * 1024;
 
 void usage(const char *prog) {
     fprintf(stderr, "usage: cat - | %s\n", prog);
 }
 
+int process(const char *line){
+    fprintf(stdout, "%s\n", line);
+    return 0;
+}
+
 int main(int argc, char* argv[]) {
-    int c = 0;
-    while ((c = getopt(argc, argv, "h")) != -1) {
-        switch (c) {
+    int ret = 0;
+    while ((ret = getopt(argc, argv, "h")) != -1) {
+        switch (ret) {
             case 'h':
                 usage(argv[0]);
                 return 0;
@@ -19,7 +28,22 @@ int main(int argc, char* argv[]) {
         }
     }
 
-    fprintf(stderr, "hello world\n");
+    int num = 0;
+    char *line = new char[max_vl];
+    while (fgets(line, max_vl, stdin) != NULL) {
+        line[strlen(line) - 1] = '\0';
+        ret = process(line);
+        if (ret != 0) {
+            fprintf(stderr, "process %s error %d\n", line, ret);
+            return -1;
+        }
+
+        num++;
+    }
+
+    fprintf(stderr, "total num: %d\n", num);
+
+    delete []line;
 
     return 0;
 }
