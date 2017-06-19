372dfe99dafcb77094c1c4ded29727da83b76e5f
Eric Wang <wangchaogo1990@gmail.com>
2017-06-19 14:18:12 +0800

    add check python template return value and hadoop conf

--- demo/python/template.py
+++ demo/python/template.py
@@ -7,11 +7,12 @@ import argparse
 import time
 
 ## debug, info, warnning, error, critical
-logging.basicConfig(level = logging.DEBUG, format = "%(levelname)s %(asctime)s [%(filename)s][%(lineno)d][%(funcName)s] %(message)s")
+logging.basicConfig(level = logging.DEBUG, format = '%(levelname)s %(asctime)s [%(filename)s][%(lineno)d][%(funcName)s] %(message)s')
 log = logging.getLogger()
 
 def process(line):
     print >> sys.stdout, line
+    return 0
 
 def main():
     parser = argparse.ArgumentParser(description = 'python template')
@@ -20,17 +21,23 @@ def main():
     arg = parser.parse_args()
 
     num = 0
-
     time_start = time.time()
     for line in sys.stdin:
         line = line.strip()
-        process(line)
+        ret = process(line)
+        if ret != 0:
+            log.fatal('process %s error', line)
+            return -1
         num = num + 1
     time_total = time.time() - time_start
-
     qps = num / time_total
-    log.info("num: %d process time: %fs qps: %d" % (num, time_total, qps))
+    log.info('num: %d process time: %fs qps: %d' % (num, time_total, qps))
+    return 0
 
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+    ret = main()
+    if ret != 0:
+        log.fatal('main error')
+        sys.exit(1)
+    sys.exit(0)
 
