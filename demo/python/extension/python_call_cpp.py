b21102a42a9446c0709064d3d22c7b9f42a13ae3
Eric Wang <wangchaogo1990@gmail.com>
2017-09-15 20:17:41 +0800

    add util to convert sign

--- demo/python/extension/python_call_cpp.py
+++ demo/python/extension/python_call_cpp.py
@@ -2,12 +2,11 @@
 #encoding: utf-8
 
 import sys
-import great_module as gm
+import util
 
-print gm.inc(1)
-print gm.str_len('abcd')
-print gm.offset('abcd')
-print gm.sign64to32('11022301334815100767') ## 1425526623,2566329514 11022301334815100767
+## 1425526623,2566329514 11022301334815100767
+print util.sign64to32('11022301334815100767')
+print util.sign32to64('1425526623,2566329514')
 
 sys.exit(0)
 
---
+++ demo/python/extension/util.cpp
@@ -0,0 +1,66 @@
+/*
+ * g++ -fPIC -shared util.cpp -o util.so -I/home/img/.jumbo/include/python2.7 -L/home/img/.jumbo/lib -lpython2.7
+ *
+ * make sure great_module.so in the same directory
+ * import great_module as gm
+ * print gm.inc(2)
+ *
+ * ref:
+ * https://docs.python.org/2/c-api/concrete.html
+ * https://docs.python.org/2/c-api/arg.html
+ */
+
+#include <string.h>
+#include <Python.h>
+
+/* tuple example */
+typedef union _node_t{
+	uint32_t sign[2];
+	uint64_t base64;
+}node_t;
+
+static PyObject *sign64to32(PyObject *self, PyObject *args){
+	char *str = NULL;
+	if (!PyArg_ParseTuple(args, "s", &str)){
+		return NULL;
+	}
+
+	node_t node;
+	sscanf(str, "%lu", &node.base64);
+	char buffer[100];
+	snprintf(buffer, sizeof(buffer), "%u,%u", node.sign[0], node.sign[1]);
+	return PyString_FromString(buffer);
+}
+
+static PyObject *sign32to64(PyObject *self, PyObject *args){
+	char *str = NULL;
+	if (!PyArg_ParseTuple(args, "s", &str)){
+		return NULL;
+	}
+
+	node_t node;
+	sscanf(str, "%u,%u", &node.sign[0], &node.sign[1]);
+	char buffer[100];
+	snprintf(buffer, sizeof(buffer), "%lu", node.base64);
+	return PyString_FromString(buffer);
+}
+
+/*
+ * Methods exposed to Python env
+ * fun in Python, fun in C++, arg, comment
+ * this list always ends with {NULL, NULL, 0, NULL}
+ */
+static PyMethodDef Methods[] = {
+	{"sign64to32", sign64to32, METH_VARARGS, "sign 64 to 32"},
+	{"sign32to64", sign32to64, METH_VARARGS, "sign 32 to 64"},
+	{NULL, NULL, 0, NULL}
+};
+
+/*
+ * export function: initgreat_module.
+ * this function name: 'init' + module name(which is also same as the file)
+ */
+PyMODINIT_FUNC initutil(void){
+	(void) Py_InitModule("util", Methods);
+}
+
