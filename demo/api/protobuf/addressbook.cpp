38842b4c30a5a8e854af353d6e8025b3c5d7e972
Eric Wang <wangchaogo1990@gmail.com>
2017-12-22 00:05:34 +0800

    add prototype

--- demo/api/protobuf/addressbook.cpp
+++ demo/api/protobuf/addressbook.cpp
@@ -5,16 +5,15 @@
 
 const int max_vl = 10 * 1024 * 1024;
 
-using namespace std;
 wcg::pb::ex::AddressBook address_book;
 
 int add_person(wcg::pb::ex::AddressBook &address_book, const char *name, const char *email, const char *phone_number);
-int print(wcg::pb::ex::AddressBook &address_book);
 
 TEST(pbTest, Test_add_person){
 	ASSERT_TRUE(add_person(address_book, "xxx", "xxx@xxx.com", "123456") == 0);
 	ASSERT_TRUE(add_person(address_book, "yyy", "yyy@yyy.com", "654321") == 0);
 	ASSERT_TRUE(address_book.person_size() == 2);
+	fprintf(stdout, "%s", address_book.Utf8DebugString().c_str());
 }
 
 TEST(pbTest, Test_string){
@@ -23,7 +22,7 @@ TEST(pbTest, Test_string){
 
 	wcg::pb::ex::AddressBook ex;
 	ASSERT_TRUE(ex.ParseFromString(v) == true);
-	print(ex);
+	fprintf(stdout, "%s", ex.Utf8DebugString().c_str());
 }
 
 TEST(pbTest, Test_char){
@@ -33,7 +32,7 @@ TEST(pbTest, Test_char){
 
 	wcg::pb::ex::AddressBook ex;
 	ASSERT_TRUE(ex.ParseFromArray(v, vl) == true);
-	print(ex);
+	fprintf(stdout, "%s", ex.Utf8DebugString().c_str());
 }
 
 TEST(pbTest, Test_file){
@@ -59,32 +58,37 @@ TEST(pbTest, Test_file){
 	fp = NULL;
 	fd = -1;
 
-	print(ex);
+	fprintf(stdout, "%s", ex.Utf8DebugString().c_str());
 }
 
-#include <fstream>
-TEST(pbTest, Test_stream){
-	const char *path = "./data.pb";
-	fstream output(path, ios::out | ios::trunc | ios::binary);
-	ASSERT_TRUE(address_book.SerializeToOstream(&output) == true);
+#include <google/protobuf/descriptor.h>
+#include <google/protobuf/message.h>
+TEST(pbTest, Test_Prototype){
+	const google::protobuf::Descriptor *descriptor = google::protobuf::DescriptorPool::generated_pool()->FindMessageTypeByName("wcg.pb.ex.AddressBook");
+	ASSERT_TRUE(descriptor != NULL);
 
-	fstream input(path, ios::in | ios::binary);
-	wcg::pb::ex::AddressBook ex;
-	ASSERT_TRUE(ex.ParseFromIstream(&input) == true);
-	print(ex);
+	const google::protobuf::Message *prototype = google::protobuf::MessageFactory::generated_factory()->GetPrototype(descriptor);
+	ASSERT_TRUE(prototype != NULL);
+
+	wcg::pb::ex::AddressBook *ab = (wcg::pb::ex::AddressBook *)prototype->New();
+
+	wcg::pb::ex::Person *person = ab->add_person();
+	person->set_name("wcg");
+	fprintf(stdout, "%s", ab->Utf8DebugString().c_str());
+	delete ab;
 }
 
 int main(int argc, char* argv[]){
 	// Verify that the version of the library that we linked against is compatible with the version of the headers we compiled against.
-// 	GOOGLE_PROTOBUF_VERIFY_VERSION;
+	GOOGLE_PROTOBUF_VERIFY_VERSION;
 
 	testing::InitGoogleTest(&argc, argv);
-	return RUN_ALL_TESTS();
+	int ret = RUN_ALL_TESTS();
 
 	// Optional: Delete all global objects allocated by libprotobuf
-// 	google::protobuf::ShutdownProtobufLibrary();
+	google::protobuf::ShutdownProtobufLibrary();
 
-// 	return 0;
+	return ret;
 }
 
 int add_person(wcg::pb::ex::AddressBook &address_book, const char *name, const char *email, const char *phone_number){
@@ -105,36 +109,6 @@ int add_person(wcg::pb::ex::AddressBook &address_book, const char *name, const c
 		case 2:
 			phone->set_type(wcg::pb::ex::WORK);
 			break;
-	}
-
-	return 0;
-}
-
-int print(wcg::pb::ex::AddressBook &address_book){
-	for(int i = 0; i < address_book.person_size(); i++){
-		const wcg::pb::ex::Person& person = address_book.person(i);
-
-		fprintf(stdout, "Name: %s\t", person.name().c_str());
-		if(person.has_email()){
-			fprintf(stdout, "email: %s\t", person.email().c_str());
-		}
-
-		for(int j = 0; j < person.phone_size(); j++){
-			const wcg::pb::ex::PhoneNumber& phone_number = person.phone(j);
-
-			switch(phone_number.type()){
-				case wcg::pb::ex::MOBILE:
-					fprintf(stdout, "Mobile phone: ");
-					break;
-				case wcg::pb::ex::HOME:
-					fprintf(stdout, "Home phone: ");
-					break;
-				case wcg::pb::ex::WORK:
-					fprintf(stdout, "Work phone: ");
-					break;
-			}
-			fprintf(stdout, "%s\n", phone_number.number().c_str());
-		}
 	}
 
 	return 0;
