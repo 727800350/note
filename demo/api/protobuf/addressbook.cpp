05c2b8971a151bd2b6a400236b6eb9915a882fcf
Eric Wang <wangchaogo1990@gmail.com>
2017-12-25 23:57:08 +0800

    add pb reflection

--- demo/api/protobuf/addressbook.cpp
+++ demo/api/protobuf/addressbook.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string>
+#include <glog/logging.h>
 #include <gtest/gtest.h>
 #include "addressbook.pb.h"
 
@@ -61,6 +62,7 @@ TEST(pbTest, Test_file){
 	fprintf(stdout, "%s", ex.Utf8DebugString().c_str());
 }
 
+// http://www.p-chao.com/2017-09-04/protobuf%E5%8F%8D%E5%B0%84%E8%AF%A6%E8%A7%A3
 #include <google/protobuf/descriptor.h>
 #include <google/protobuf/message.h>
 TEST(pbTest, Test_Prototype){
@@ -71,6 +73,7 @@ TEST(pbTest, Test_Prototype){
 	ASSERT_TRUE(prototype != NULL);
 
 	wcg::pb::ex::AddressBook *ab = (wcg::pb::ex::AddressBook *)prototype->New();
+	ASSERT_TRUE(ab != NULL);
 
 	wcg::pb::ex::Person *person = ab->add_person();
 	person->set_name("wcg");
@@ -78,9 +81,30 @@ TEST(pbTest, Test_Prototype){
 	delete ab;
 }
 
+TEST(pbTest, Test_Reflector){
+	wcg::pb::ex::AddressBook ab;
+	const google::protobuf::Descriptor* descriptor = ab.GetDescriptor();
+	ASSERT_TRUE(descriptor != NULL);
+
+	const google::protobuf::Reflection* reflecter = ab.GetReflection();
+	ASSERT_TRUE(reflecter != NULL);
+
+	const google::protobuf::FieldDescriptor* owner = descriptor->FindFieldByName("owner");
+	ASSERT_TRUE(owner != NULL);
+	reflecter->SetString(&ab, owner, "Eric Wang");
+
+	const google::protobuf::FieldDescriptor* desc = descriptor->FindFieldByName("desc");
+	reflecter->SetString(&ab, desc, "an address book example");
+
+	fprintf(stdout, "%s", ab.Utf8DebugString().c_str());
+}
+
 int main(int argc, char* argv[]){
 	// Verify that the version of the library that we linked against is compatible with the version of the headers we compiled against.
 	GOOGLE_PROTOBUF_VERIFY_VERSION;
+
+	address_book.set_owner("Eric Wang");
+	address_book.set_desc("an address book example");
 
 	testing::InitGoogleTest(&argc, argv);
 	int ret = RUN_ALL_TESTS();
