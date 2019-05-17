7efc5ceaec1097609e728b69359b88d394312784
Eric Wang <wangchaogo1990@gmail.com>
2019-05-17 05:16:25 +0000

    code style

--- demo/python/template.py
+++ demo/python/template.py
@@ -17,19 +17,19 @@ parser.add_argument('--dict', action = 'store', dest = 'dict', default = 'dict.t
 arg = parser.parse_args()
 
 def process(line):
-	print(line)
-	return 0
+  print(line)
+  return 0
 
 
 num = 0
 time_start = time.time()
 for line in sys.stdin:
-	line = line.replace(os.linesep, '')
-	ret = process(line)
-	if ret != 0:
-		log.error('process %s error', line)
-		sys.exit(1)
-	num = num + 1
+  line = line.replace(os.linesep, '')
+  ret = process(line)
+  if ret != 0:
+    log.error('process %s error', line)
+    sys.exit(1)
+  num = num + 1
 time_total = time.time() - time_start
 qps = num / time_total
 log.info('num: %d process time: %fs qps: %d' % (num, time_total, qps))
